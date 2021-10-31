3adc45cfad7c6cfc01d1ae3dfd74128218fe6115
fetsiura <fetsiura@gmail.com>
2021-10-31 13:11:02 +0100

    some

--- src/main/java/pl/coderslab/homeworks/mysql/second/Main04.java
+++ src/main/java/pl/coderslab/homeworks/mysql/second/Main04.java
@@ -1,18 +1,29 @@
 package pl.coderslab.homeworks.mysql.second;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Collection;
 
 public class Main04 {
 
     public static void main(String[] args) {
 
-
+        printMovies();
 
     }
 
-    public void printMovies (){
+    public static void printMovies (){
 
-        String all = "select * from movies";
+        String all = "select title, description,rating,c.name from movies as m, cinemas as c\n" +
+                "join  cinemas_movies cm on c.id = cm.cinema_id";
+
+        try(Connection connection = DBUtil.connect()){
+
+            DBUtil.showAll(connection,all);
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
 
     }
 
